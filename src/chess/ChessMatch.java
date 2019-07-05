eaeda4c1107d4c6a8fe56f1f22abcad75ed44c22
fellipySaldanha <fellipy.saldanha@gmail.com>
2019-07-04 22:14:55 -0300

    implementing possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -30,9 +30,10 @@ public class ChessMatch {
 		Position source = sourcePosition.toPosition();
 		Position target = targePosition.toPosition();
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece)capturedPiece;
-	}
+	}	
 
 	private void validateSourcePosition(Position position){
 		if(!board.thereIsAPiece(position)){
@@ -40,6 +41,12 @@ public class ChessMatch {
 		}
 		if(!board.piece(position).isThereAnyPossibleMove()){
 			throw new ChessException("There is no póssible moves for the chosen piece");
+		}
+	}
+
+	private void validateTargetPosition(Position source, Position target){
+		if(!board.piece(source).possibleMove(target)){
+			throw new ChessException("The chosen piece can't move to target position");
 		}
 	}
 
